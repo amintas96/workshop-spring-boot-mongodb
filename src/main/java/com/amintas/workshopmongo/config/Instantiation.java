93a65fa8b50303514e7b56de6ac74e2c952f1f16
amintas96 <amintaspereira@hotmail.com>
2022-01-11 09:50:54 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/amintas/workshopmongo/config/Instantiation.java
+++ src/main/java/com/amintas/workshopmongo/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.amintas.workshopmongo.dto.AuthorDTO;
+import com.amintas.workshopmongo.dto.CommentDTO;
 import com.amintas.workshopmongo.entities.Post;
 import com.amintas.workshopmongo.entities.User;
 import com.amintas.workshopmongo.repositories.PostRepository;
@@ -42,10 +43,18 @@ public class Instantiation implements CommandLineRunner {
 				"Vou viajar agora para São Paulo, abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().add(c3);
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
+
 	}
 
 }
