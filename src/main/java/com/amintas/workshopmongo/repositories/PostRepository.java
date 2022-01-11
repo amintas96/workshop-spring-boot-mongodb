b2c50b70e785fa2a8b0343dbbe55a7bcfc71e269
amintas96 <amintaspereira@hotmail.com>
2022-01-11 10:56:43 -0300

    Consulta simples com @Query

--- src/main/java/com/amintas/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/amintas/workshopmongo/repositories/PostRepository.java
@@ -3,13 +3,16 @@ package com.amintas.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.amintas.workshopmongo.entities.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
+public interface PostRepository extends MongoRepository<Post, String> {
 
-	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 }
--- src/main/java/com/amintas/workshopmongo/resources/util/URL.java
+++ src/main/java/com/amintas/workshopmongo/resources/util/URL.java
@@ -3,8 +3,6 @@ package com.amintas.workshopmongo.resources.util;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
-import org.springframework.data.mongodb.core.aggregation.StringOperators.ToLower;
-
 public class URL {
 
 	public static String decodeParam(String text) {
